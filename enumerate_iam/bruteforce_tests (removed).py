ff281e442a8eb81af0f017be8033aab8b35c8cf9
Chris Rothwell <me@chrisrothwell.com>
2022-01-15 15:51:25 +0700

    sync

---
+++ enumerate_iam/bruteforce_tests (removed).py
@@ -0,0 +1,18 @@
+removed:
+
+    "globalaccelerator": [
+        "describe_accelerator_attributes",
+        "list_accelerators"
+    ],
+        "devicefarm": [
+        "get_account_settings",
+        "get_offering_status",
+        "list_device_instances",
+        "list_devices",
+        "list_instance_profiles",
+        "list_offering_promotions",
+        "list_offering_transactions",
+        "list_offerings",
+        "list_projects",
+        "list_vpce_configurations"
+    ],
